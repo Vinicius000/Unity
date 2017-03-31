0990ce1c8dece6b31df203c741200a90d7ece2ec
Tais Souza <ttisas25@gmail.com>
2017-03-31 18:45:18 -0300

    Adicionando os scripts - errado

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public Estado estado { get; private set; }
+
+    public GameObject obstaculo;
+    public float espera;
+    public float tempoDestruicao;
+
+
+    public static GameController instancia = null;
+
+    void Awake() {
+        if (instancia == null) {
+            instancia = this;
+        }
+        else if (instancia != null) {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start() {
+        estado = Estado.AguardandoComecar;
+    }
+
+    IEnumerator GerarObstaculos() {
+        while (GameController.instancia.estado == Estado.Jogando) {
+            Vector3 pos = new Vector3(12.13f, Random.Range(-5.0f, -1f), 44.5f);
+            GameObject obj = Instantiate(obstaculo, pos, Quaternion.identity) as GameObject;
+            Destroy(obj, tempoDestruicao);
+            yield return new WaitForSeconds(espera);
+        }
+    }
+
+    public void PlayerComecou() {
+        estado = Estado.Jogando;
+        StartCoroutine(GerarObstaculos());
+    }
+
+    public void PlayerMorreu() {
+        estado = Estado.GameOver;
+    }
+}
+
+
